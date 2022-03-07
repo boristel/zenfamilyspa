791c54d8a4352a10c242115988dd8f13e54deeb7
Borist <borist.nababan@gmail.com>
2022-03-07 15:00:38 +0700

    update server.js

--- config/server.js
+++ config/server.js
@@ -2,6 +2,6 @@ module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: env.array('bgKomDv15VFkrs+/bWRHHA==', 'mHkXOYtGe3W75ZM/JbRK6w==','02IMi0at3bRc2yLin+EDsg==', 'FzCbFo8flhpPYAg5+suGQw=='),
   },
 });
