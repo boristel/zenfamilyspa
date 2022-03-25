dce8a54aae70eadeee451566142fb958615a5a63
Borist <borist.nababan@gmail.com>
2022-03-25 18:34:00 +0700

    update server url

--- config/server.js
+++ config/server.js
@@ -3,6 +3,5 @@ module.exports = ({ env }) => ({
   port: env.int('PORT', 1337),
   app: {
     keys: env.array('bgKomDv15VFkrs+/bWRHHA==', 'mHkXOYtGe3W75ZM/JbRK6w==','02IMi0at3bRc2yLin+EDsg==', 'FzCbFo8flhpPYAg5+suGQw=='),
-  },
-  url: 'https://zenfamilyspa-tiw3n.ondigitalocean.app/'
+  }
 });
